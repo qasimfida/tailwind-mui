f3cbb03d4235761023c0f68dbfc1ddb121ced3d7
Qasim Fida <qasimfida0054@gmail.com>
2023-03-17 15:49:38 +0500

    theme file

--- pages/addcontract.tsx
+++ pages/addcontract.tsx
@@ -1,22 +1,17 @@
 import { Container } from "@material-ui/core";
-import SettingsIcon from "@mui/icons-material/Settings";
 import * as React from "react";
 import Tabs from "../components/Addcontract/Tabs";
 export default function addContract() {
   return (
-    <>
-    <Container>
+    <Container maxWidth="lg" >
 
-      <div className="w-full max-w-[1700px] mx-auto p-4">
         <div className="pl-[36px] mb-7">
           <span className="text-4xl text-gray-700">Text here</span>
         </div>
         <div className="">
           <Tabs />
         </div>
-      </div>
     </Container>
-    </>
   );
 }
 
